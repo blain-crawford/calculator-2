347bd72c439233fe2c6d11f45c2978dd88ebad4c
Blain Crawford <blaincrawford@gmail.com>
2022-01-05 15:33:53 -0500

    starting to label js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,21 @@
-//DOM element and Global Variable creation
-const clearButton = document.querySelector("#clear");
-const numberButtons = document.querySelectorAll(".number-button");
-const operatorButtons = document.querySelectorAll(".operator-button");
-const equationText = document.querySelector("#running-equation");
-const equalsButton = document.getElementById("=");
-const solutionText = document.querySelector("#solution");
-const squareRootButton = document.getElementById("√");
-const squaredButton = document.getElementById('**')
-let runningSolution = "";
+/**
+ * DOM element and Global Variable creation
+ */
+const clearButton = document.querySelector('#clear');
+const numberButtons = document.querySelectorAll('.number-button');
+const operatorButtons = document.querySelectorAll('.operator-button');
+const equationText = document.querySelector('#running-equation');
+const equalsButton = document.getElementById('=');
+const solutionText = document.querySelector('#solution');
+const squareRootButton = document.getElementById('√');
+const squaredButton = document.getElementById('**');
+let runningSolution = '';
 let equationArray = [];
-let tempNumber = "";
+let tempNumber = '';
 
-//change strings to float or integer
+/**
+ * change strings to float or integer
+ */
 let generateNumber = function (string) {
   if (string - Math.floor(string) !== 0) {
     return parseFloat(string);
@@ -20,91 +24,92 @@ let generateNumber = function (string) {
   }
 };
 
-//Make Sure floats never get past a certain length
-let roundFloat = function(number){
+/**
+ * Make Sure floats never get past a certain length
+ */
+let roundFloat = function (number) {
   let numString = number.toString();
   numString = numString.substring(numString.indexOf('.'), numString.length - 0);
-  if(number - Math.floor(number) !== 0 && numString.length > 10){
-      number = number.toFixed(10);
-      number = parseFloat(number);
-      numstring = number.toString();
+  if (number - Math.floor(number) !== 0 && numString.length > 10) {
+    number = number.toFixed(10);
+    number = parseFloat(number);
+    numstring = number.toString();
   }
- 
   for (let i = 1; i < numString.length; i++) {
-      let currentIndex = numString[i];
-      let nextIndex = numString[i + 1];
-      if(currentIndex === nextIndex){
-          return parseFloat(number.toFixed(i));
-      }
+    let currentIndex = numString[i];
+    let nextIndex = numString[i + 1];
+    if (currentIndex === nextIndex) {
+      return parseFloat(number.toFixed(i));
+    }
   }
-  return number;   
+  return number;
 };
 
-//mathWork functionality
+/**
+ * mathWork functionality
+ */
 const mathWork = function (opperand1, operator, opperand2) {
-  if (operator === "+") {
+  if (operator === '+') {
     return opperand1 + opperand2;
-  } else if (operator === "-") {
+  } else if (operator === '-') {
     return opperand1 - opperand2;
-  } else if (operator === "*") {
+  } else if (operator === '*') {
     return opperand1 * opperand2;
-  } else if (operator === "/") {
+  } else if (operator === '/') {
     return opperand1 / opperand2;
   }
 };
 
-//functionality for number buttons
+/**
+ * functionality for number buttons
+ */
 const numberInput = function () {
+
   //starts new equation after the equals button is pressed
-  if(
-    equationArray.length === 1 && 
-    tempNumber === ''
-  ){
+  if (equationArray.length === 1 && tempNumber === '') {
     equationArray = [];
     equationText.textContent = '';
   }
+  
   if (
-    this.id === "." &&
-    tempNumber.includes(".") &&
-    equationText.textContent.includes(".")
+    this.id === '.' &&
+    tempNumber.includes('.') &&
+    equationText.textContent.includes('.')
   ) {
     return;
   } else {
     tempNumber += this.id;
     equationText.textContent += this.id;
   }
-
 };
 
 //Adding functionality for clear button
 const clearCalculator = function () {
-  if (this.id === "clear") {
-    runningSolution = "";
+  if (this.id === 'clear') {
+    runningSolution = '';
     equationArray = [];
-    tempNumber = "";
-    equationText.textContent = "";
-    solutionText.textContent = "";
+    tempNumber = '';
+    equationText.textContent = '';
+    solutionText.textContent = '';
   }
 };
 
 //functionality for square root button
 let squareRoot = function () {
-  if (tempNumber === "" && equationArray.length === 0) {
+  if (tempNumber === '' && equationArray.length === 0) {
     return;
-  } else if (equationArray.length === 2 && tempNumber !== "") {
-    equationArray.push(generateNumber(tempNumber))
-    let mySolution = Math.sqrt(mathWork(
-      equationArray[0],
-      equationArray[1],
-      equationArray[2]
-    ));
+  } else if (equationArray.length === 2 && tempNumber !== '') {
+    equationArray.push(generateNumber(tempNumber));
+    let mySolution = Math.sqrt(
+      mathWork(equationArray[0], equationArray[1], equationArray[2])
+    );
     equationArray = [];
     equationArray.push(mySolution);
     runningSolution = mySolution;
-    solutionText.textContent = "";
+    solutionText.textContent = '';
     equationText.textContent = `${mySolution}`;
-    tempNumber = "";
-  } else if (equationArray.length <= 2 && tempNumber === "") {
+    tempNumber = '';
+  } else if (equationArray.length <= 2 && tempNumber === '') {
     // equationArray.push(tempNumber);  //CAN DELETE???\\
     let mySolution = Math.sqrt(generateNumber(equationArray[0]));
     if (mySolution - Math.floor(mySolution) !== 0) {
@@ -113,10 +118,10 @@ let squareRoot = function () {
     equationArray = [];
     equationArray.push(mySolution);
     runningSolution = mySolution;
-    solutionText.textContent = "";
+    solutionText.textContent = '';
     equationText.textContent = `${mySolution}`;
-    tempNumber = "";
-  } else if (equationArray.length === 0 && tempNumber !== "") {
+    tempNumber = '';
+  } else if (equationArray.length === 0 && tempNumber !== '') {
     equationArray.push(tempNumber);
     let mySolution = Math.sqrt(generateNumber(equationArray[0]));
     if (mySolution - Math.floor(mySolution) !== 0) {
@@ -125,35 +130,33 @@ let squareRoot = function () {
     equationArray = [];
     equationArray.push(mySolution);
     runningSolution = mySolution;
-    solutionText.textContent = "";
+    solutionText.textContent = '';
     equationText.textContent = `${mySolution}`;
-    tempNumber = "";
-  } else if (equationArray.length === 1 && tempNumber === ''){
+    tempNumber = '';
+  } else if (equationArray.length === 1 && tempNumber === '') {
     let mySolution = Math.sqrt(generateNumber(equationArray[0]));
 
     if (mySolution - Math.floor(mySolution) !== 0) {
       mySolution = mySolution.toFixed(10);
     }
-    
     equationArray = [];
     equationArray.push(mySolution);
     runningSolution = mySolution;
-    solutionText.textContent = "";
+    solutionText.textContent = '';
     equationText.textContent = `${mySolution}`;
-    tempNumber = "";
+    tempNumber = '';
   }
 };
 
 //functionality for squared button
-let squared = function(){
-  if(tempNumber === '' && equationArray.length === 0){
+let squared = function () {
+  if (tempNumber === '' && equationArray.length === 0) {
     return;
   } else if (tempNumber !== '' && equationArray.length === 0) {
     let mySolution = generateNumber(tempNumber) ** 2;
     equationArray.push(mySolution);
     equationText.textContent = mySolution;
     tempNumber = '';
-
   } else if (tempNumber === '' && equationArray.length > 0) {
     let mySolution = generateNumber(equationArray[0]) ** 2;
     equationArray = [];
@@ -162,23 +165,20 @@ let squared = function(){
     tempNumber = '';
   } else if (tempNumber !== '' && equationArray.length <= 2) {
     equationArray.push(generateNumber(tempNumber));
-    let mySolution = mathWork(
-      equationArray[0],
-      equationArray[1],
-      equationArray[2]
-    ) ** 2;
+    let mySolution =
+      mathWork(equationArray[0], equationArray[1], equationArray[2]) ** 2;
     equationArray = [];
     equationArray.push(mySolution);
     equationText.textContent = mySolution;
     tempNumber = '';
-
   }
-}
+};
 
 //Functionality for opperand buttons
 let addInitialOperator = function () {
+
   //replaces opperand in equation if one is entered twice
-  if (equationArray.length === 2 && tempNumber === "") {
+  if (equationArray.length === 2 && tempNumber === '') {
     if (equationArray[1] === this.id) {
       return;
     } else {
@@ -188,10 +188,9 @@ let addInitialOperator = function () {
     }
     return;
   }
-
   if (equationArray.length > 1) {
     equationArray.push(generateNumber(tempNumber));
-    tempNumber = "";
+    tempNumber = '';
     let mySolution = mathWork(
       equationArray[0],
       equationArray[1],
@@ -207,11 +206,10 @@ let addInitialOperator = function () {
     solutionText.textContent = mySolution;
     equationText.textContent = `${mySolution} ${this.id} `;
   }
-
   if (tempNumber.length > 0 && equationArray.length === 0) {
     equationArray.push(generateNumber(tempNumber));
     equationArray.push(this.id);
-    tempNumber = "";
+    tempNumber = '';
     equationText.textContent += ` ${this.id} `;
   }
   if (tempNumber.length === 0 && equationArray.length === 1) {
@@ -227,14 +225,17 @@ let equals = function () {
   }
 
   equationArray.push(generateNumber(tempNumber));
-  tempNumber = "";
-  solutionText.textContent = "";
+  tempNumber = '';
+  solutionText.textContent = '';
   let mySolution = mathWork(
     equationArray[0],
     equationArray[1],
     equationArray[2]
   );
-  if (mySolution - Math.floor(mySolution) !== 0 && mySolution.toString().length > 10) {
+  if (
+    mySolution - Math.floor(mySolution) !== 0 &&
+    mySolution.toString().length > 10
+  ) {
     mySolution = roundFloat(mySolution);
   }
   equationText.textContent = mySolution;
@@ -242,24 +243,19 @@ let equals = function () {
   equationArray.push(mySolution);
 };
 
-//adding functionality to number buttons
+//adding functionality to buttons
 numberButtons.forEach((button) =>
-  button.addEventListener("click", numberInput)
+  button.addEventListener('click', numberInput)
 );
 
-//adding functionality to clear button
-clearButton.addEventListener("click", clearCalculator);
+clearButton.addEventListener('click', clearCalculator);
 
-//adding Event Listener for squre root button
-squareRootButton.addEventListener("click", squareRoot);
+squareRootButton.addEventListener('click', squareRoot);
 
-//adding event listener for squared button
 squaredButton.addEventListener('click', squared);
 
-//adding operator button functionality
 operatorButtons.forEach((button) =>
-  button.addEventListener("click", addInitialOperator)
+  button.addEventListener('click', addInitialOperator)
 );
 
-//adding functionality to equals button
-equalsButton.addEventListener("click", equals);
+equalsButton.addEventListener('click', equals);
